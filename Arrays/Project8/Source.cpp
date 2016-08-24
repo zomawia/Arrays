d21fcf2229b31b54d8081c7cab56dd06a94dc96c
Zomawia Sailo <zomawia@gmail.com>
2016-08-24 11:57:58 -0700

    TEST

---
+++ Arrays/Project8/Source.cpp
@@ -0,0 +1,19 @@
+// Zomawia Sailo
+// zomawia@gmail.com
+
+//8. Create a program that declares an array of 5 integer values.These values will represent the
+//	health of 5 characters.Initialise the values all to 100. Your program is to ask the user to
+//	enter a number(0 to 4) to select a character that is to be attacked by a monster.Each attack
+//	deals 40 damage per attack.If a player’s health gets below 0 then should not be able to be
+//	attacked.Your program should simulate 5 attacks.After the attacks have finished, output
+//	the parties’ remaining health to the console.
+
+//	CHALLENGE: Research random number generation and modify your program so that random
+//	damage is dealt with each attack.
+
+#include <iostream>
+
+int main()
+{
+
+}
