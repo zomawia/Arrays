ba60da9d26febde1a350528b804f6d3637f2cf96
zomawia <zomawia@gmail.com>
2016-08-24 17:55:40 -0700

    Problem 8 submitted with random number generator used.

--- Arrays/Project8/Source.cpp
+++ Arrays/Project8/Source.cpp
@@ -1,7 +1,7 @@
 // Zomawia Sailo
 // zomawia@gmail.com
 
-//8. Create a program that declares an array of 5 integer values.These values will represent the
+//8. Create a program that declares an array of 5 integer values. These values will represent the
 //	health of 5 characters.Initialise the values all to 100. Your program is to ask the user to
 //	enter a number(0 to 4) to select a character that is to be attacked by a monster.Each attack
 //	deals 40 damage per attack.If a player’s health gets below 0 then should not be able to be
@@ -12,8 +12,49 @@
 //	damage is dealt with each attack.
 
 #include <iostream>
+#include <time.h>
 
 int main()
 {
+	int partyHealth[5] = { 100,100,100,100,100 };
+	int input = 0, monsterAttack = 0;
 
+
+	printf("Your party is ambushed by a bee-shooting dog.\n");
+
+	printf("Who will take a bee shot for the greater good of the party?\n");
+
+	printf("[0] Granpilf the Goop\t[1] Furious Fork  [2] Pretty Boy Sand\n[3] Short Guns George [4] Harambe\n>>");
+
+	for (int i = 0; i < 5; ++i)
+	{
+		scanf_s("%d", &input);
+
+		if (input > 4 || input < 0)
+		{
+			printf("Invalid selection. Select another player\n", input);
+			i--;
+			
+		}
+		else if (partyHealth[input] < 0)
+		{
+			printf("Player %d is incapacitated! Select another player\n", input);
+			i--;
+			
+		}		
+		else
+		{
+			partyHealth[input] -= monsterAttack = rand() % 100; //random monster attack damage
+			printf("Player %d rushes forward to defend. Received %d damaged.\n", input, monsterAttack);
+					
+		}
+		printf("Who will take the next bee shot?\n>>");
+	}
+
+	for (int i = 0; i < 5; i++)
+	{
+		printf("\nPlayer %d Health: %d\n", i, partyHealth[i]);
+	}
+
+	system("pause");
 }
